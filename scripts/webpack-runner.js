32d0b1a5853e3771546229c7225ee748cc171fa5
Alireza Poodineh <itsaeliux@gmail.com>
2026-06-26 12:50:48 +0330

    Refactor build process
    Signed-off-by: Alireza Poodineh <itsaeliux@gmail.com>

---
+++ scripts/webpack-runner.js
@@ -0,0 +1,19 @@
+// scripts/webpack-runner.js
+const { spawn } = require('child_process');
+
+const config = process.argv[2] || 'webpack.config.js';
+const env = process.env.NODE_ENV || 'development';
+const target = process.env.WEBQ_TARGET || 'browser';
+
+const args = ['--config', config, '--progress'];
+if (env === 'production') {
+  args.push('--fail-on-warnings');
+}
+
+console.log(`> webpack ${args.join(' ')} (NODE_ENV=${env}, WEBQ_TARGET=${target})`);
+const child = spawn('npx', ['webpack', ...args], {
+  stdio: 'inherit',
+  env: { ...process.env, NODE_ENV: env, WEBQ_TARGET: target },
+});
+
+child.on('exit', (code) => process.exit(code));
