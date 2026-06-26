b4eebbeb628420a3231026730df5d2f4ee2aa6e2
Alireza Poodineh <itsaeliux@gmail.com>
2026-06-26 17:27:55 +0330

    Add electron-builder js config
    Signed-off-by: Alireza Poodineh <itsaeliux@gmail.com>

---
+++ electron-builder.config.js
@@ -0,0 +1,27 @@
+const pkg = require('./package.json');
+
+module.exports = {
+  appId: pkg.name,
+  artifactName: '${productName}.${ext}',
+  asar: true,
+  detectUpdateChannel: false,
+  electronLanguages: 'en',
+  productName: pkg.displayName,
+  directories: {
+    buildResources: 'assets',
+    output: 'dist',
+  },
+  files: ['www/**/*', '!node_modules/**/*'],
+  appImage: {
+    compression: 'zstd',
+  },
+  linux: {
+    icon: 'assets/icon.png',
+    target: ['AppImage'],
+    category: 'Utility',
+    syncDesktopName: true,
+  },
+  win: {
+    target: ['portable'],
+  },
+};
