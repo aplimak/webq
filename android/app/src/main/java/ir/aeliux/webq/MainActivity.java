34b9621b28030a93953407c48ca0bbf56635e668
Alireza Poodineh <itsaeliux@gmail.com>
2026-07-11 17:41:53 +0330

    Enable edgetoedge on most devices
    Signed-off-by: Alireza Poodineh <itsaeliux@gmail.com>

--- android/app/src/main/java/ir/aeliux/webq/MainActivity.java
+++ android/app/src/main/java/ir/aeliux/webq/MainActivity.java
@@ -1,5 +1,16 @@
 package ir.aeliux.webq;
 
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.core.view.WindowCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowCompat.enableEdgeToEdge(getWindow());
+    }
+}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,6 @@ const config: CapacitorConfig = {
   appId: 'ir.aeliux.webq',
   appName: 'WebQ',
   webDir: 'bundle/cap',
-  plugins: {
-    StatusBar: {
-      overlaysWebView: true,
-    },
-  },
 };
 
 export default config;
